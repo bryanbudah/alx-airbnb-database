dc06a9ad030ad9eec0060e7e3fc4b75b65e965c6
Bryan J Kariuki <webdeveloperjoekariuki@gmail.com>
2025-07-09 23:34:28 -0700

    Add ROW_NUMBER() window function to rank properties by total bookings

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -38,3 +38,10 @@ GROUP BY
     property.id, property.title
 ORDER BY
     booking_rank;
+-- Rank properties based on number of bookings
+SELECT 
+    property_id,
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM bookings
+GROUP BY property_id;
